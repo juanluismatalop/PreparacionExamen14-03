35673d069d0b9254d699ac025b218f56171e8a4d
julu <juanluismatalop@gmail.com>
2024-03-12 10:06:19 +0100

    12/3/24

---
+++ src/Polimorfismo/Ejercicio1/ArrayReales.java
@@ -0,0 +1,12 @@
+package Polimorfismo.Ejercicio1;
+
+public class ArrayReales {
+    private double[] datos;
+    public ArrayReales(double[] datos) {
+        this.datos = datos;
+    }
+    public double[] getDatos() {
+        return datos;
+    }
+
+}
---
+++ src/Polimorfismo/Ejercicio1/Estadistica.java
@@ -0,0 +1,10 @@
+package Polimorfismo.Ejercicio1;
+
+public interface Estadistica {
+    int obtenerNumeroValores(); //n´umero de elementos de una colecci´on
+    double obtenerValorMinimo(); //valor m´ınimo de una colecci´on
+    double obtenerValorMaximo(); //valor m´aximo de una colecci´on
+    double calcularSuma(); //suma de los valores de una colecci´on
+    double calcularValorMedio(); //valor medio de de una colecci´on
+    double calcularDesviacionTipica(); //desviaci´on t´ıpica de de una colecci´on.
+}
