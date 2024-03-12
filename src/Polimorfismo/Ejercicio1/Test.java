955813512d20331c06a9737c58542f9b4807adc5
julu <juanluismatalop@gmail.com>
2024-03-12 14:22:58 +0100

    12/3/24

--- src/Polimorfismo/Ejercicio1/Test.java
+++ src/Polimorfismo/Ejercicio1/Test.java
@@ -1,10 +1,16 @@
 package Polimorfismo.Ejercicio1;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class Test {
     public static void main(String[] args) {
         double[] numbers = new Random().doubles(0,350).limit(10).toArray();
+        //System.out.println(Arrays.stream(numbers).);
+        System.out.println(Arrays.stream(numbers).max());
+        System.out.println(Arrays.stream(numbers).min());
+        System.out.println(Arrays.stream(numbers).sum());
+        System.out.println(Arrays.stream(numbers).average());
 
     }
 }
